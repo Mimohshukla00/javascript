942945009d766fbaa4f643c0b61f23c535be6c75
mimoh shukla <145251754+Mimohshukla00@users.noreply.github.com>
2023-10-05 18:31:48 +0000

    js1 config done

---
+++ js1/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+//treat all js code as newer version
+// alert (3+3) we are using nodejs not browser
+console.log('mimoh')
+let name ='mimoh'
+let age=18
+let islogdin =false
+// number BigInt string Boolean=>true,false null undefined 
+
