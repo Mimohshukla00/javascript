2063aa96687c28c002784c13c5468bb6c65fa522
mimoh shukla <145251754+Mimohshukla00@users.noreply.github.com>
2023-10-25 17:07:53 +0000

    add arrow function and iife

--- arrowfunctions.js
+++ arrowfunctions.js
@@ -1,21 +1,34 @@
+// all about arrow function
 const user = {
-    username:"mimoh",
-    price:9999,
-    welcomemessage:function(){
-        console.log(`${ this.username},welcome to website`);
+    username:"mimohshukla",
+    price:999,
+    welcomeMessage :function(){
+        console.log(`${this.username},welcome to website`)
+        //console.log(this)
     }
 }
-// user.welcomemessage()
-// user.username = "sukla"
+// user.welcomeMessage()
+// user.welcomeMessage= "hitesh";
+// //user.welcomeMessage()
+// //this>= tells us about current contest
+// //console.log(this)
 // function chai(){
-//     let username = "mimoh"
 //     console.log(this);
 // }
 // chai()
-
-
-// const chai = function(){
-//     let username = "hitesh"
-//     console.log(this.username);
+// const mimoh =() =>{
+//     let username ="hitesh"
+//     console.log(this);
 // }
+// mimoh()
+// const addTwo =(num1,num2) =>{
+//     return num1+num2
+// }
+// console.log(addTwo(3,4))
+//in this type we should defenitely write return
+//=> if this is used then we should avoid return
+// const addTwo =(num1,num2) => (num1 + num2)
+// console.log(addTwo(3,4))
+// const myArray = [2,3,4,5,6]
 
+// myArray.forEach()
---
+++ iife.js
@@ -0,0 +1,6 @@
+//Immediately Invoked Function Expressions
+(function mimoh(){
+    console.log(`DB CONNECTED`);
+})();
+//semicolon is must
+
