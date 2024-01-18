1a14b42bd0d509e57b8d7b0c66e3506805fac6f9
mimoh shukla <145251754+Mimohshukla00@users.noreply.github.com>
2024-01-18 22:02:52 +0530

    config all

---
+++ Javascript/index.js
@@ -0,0 +1,23 @@
+// function createRectangle(len,bre){
+//     return rectangle = {
+//         length:len,
+//         breadth:bre,
+//         draw() {
+//             console.log('draw')
+//         }
+//     }
+// }
+// let rectangleObj1 = createRectangle(2,5);
+// 
+// function createRectangle(len,bre){
+//     return rectangle ={
+//         length:len,
+//         breadth:bre,
+//         draw(){
+//             console.log('draw')
+//         }
+//     }
+// }
+// let rectangleObj1=createRectangle(2,5);
+//function by constructor function
+//
